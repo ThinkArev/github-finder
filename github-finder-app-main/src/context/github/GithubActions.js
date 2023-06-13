11b4c9bead31cf50cc4cdf08bd4b56299355a508
Gagan <mgagan109@gmail.com>
2023-06-13 11:20:27 +0530

    github url updated

--- github-finder-app-main/src/context/github/GithubActions.js
+++ github-finder-app-main/src/context/github/GithubActions.js
@@ -3,8 +3,8 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 const github = axios.create({
-  baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  baseURL: 'https://api.github.com',
+  headers: { Authorization: `token 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` },
 })
 
 // Get search results
